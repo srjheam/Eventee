ebef0e40a3ea3608f780e631049851a88bdf2b98
Jheam Storch Ross <jheam.storch@hotmail.com>
2023-09-18 23:51:59 -0300

    Add User

---
+++ Eventee.Api/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eventee.Api.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public ICollection<GetTogether> HostedGetTogethers { get; set; }
+    public ICollection<GetTogether> SubscribedGetTogethers { get; set; }
+}
