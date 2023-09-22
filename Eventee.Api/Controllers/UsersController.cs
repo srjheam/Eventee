2cd51a9877c1ef67b64520a54cf31361b4fd65d2
Jheam Storch Ross <jheam.storch@hotmail.com>
2023-09-21 21:49:14 -0300

    Add UsersController

--- Eventee.Api/Controllers/UsersController.cs
+++ Eventee.Api/Controllers/UsersController.cs
@@ -1,97 +1,142 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Eventee.Api.Data;
-using Eventee.Api.Models;
+using AutoMapper;
+using Eventee.Api.Controllers.Dtos;
+using Eventee.Api.Filters;
+using Eventee.Api.Wrappers;
+using Eventee.Api.Factories;
 
-namespace Eventee.Api.Controllers
+namespace Eventee.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    private readonly EventeeContext _context;
+
+    private readonly IPagedResponseFactory _pagedResponseFactory;
+
+    private readonly IMapper _mapper;
+
+    public UsersController(EventeeContext context, IPagedResponseFactory pagedResponseFactory, IMapper mapper)
     {
-        private readonly EventeeContext _context;
+        _context = context;
 
-        public UsersController(EventeeContext context)
-        {
-            _context = context;
-        }
+        _pagedResponseFactory = pagedResponseFactory;
 
-        // GET: api/Users
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
-        {
-            return await _context.Users.ToListAsync();
-        }
+        _mapper = mapper;
+    }
 
-        // GET: api/Users/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser(int id)
-        {
-            var user = await _context.Users.FindAsync(id);
+    // GET: api/Users
+    [HttpGet]
+    public async Task<ActionResult<PagedResponse<UserDto>>> GetUsers([FromQuery] PaginationFilter paginationFilter)
+    {
+        var dataSlice = await _context.Users
+            .Skip((paginationFilter.PageNumber - 1) * paginationFilter.Count)
+            .Take(paginationFilter.Count)
+            .ToArrayAsync();
 
-            if (user == null)
-                return NotFound();
+        var dto = _mapper.Map<UserDto[]>(dataSlice);
 
-            return user;
-        }
+        return Ok(_pagedResponseFactory.CreatePagedReponse(dto, paginationFilter, Request.Path.Value ?? ""));
+    }
 
-        // PUT: api/Users/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(int id, User user)
-        {
-            if (id != user.Id)
-                return BadRequest();
+    // GET: api/Users/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Response<UserDto>>> GetUser(string id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user is null)
+            return NotFound();
 
-            _context.Entry(user).State = EntityState.Modified;
+        var dto = _mapper.Map<UserDto>(user);
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        return Ok(new Response<UserDto>(dto));
+    }
 
-            return NoContent();
-        }
+    [HttpGet("{id}/hostings")]
+    public async Task<ActionResult<PagedResponse<GetTogetherDto>>> GetUserAllHostings(string id, [FromQuery] PaginationFilter paginationFilter)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user is null)
+            return NotFound(new Response<string>("User not found."));
 
-        // DELETE: api/Users/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUser(int id)
-        {
-            if (_context.Users == null)
-            {
-                return NotFound();
-            }
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
+        var dataSlice = await _context.GetTogethers
+            .Include(e => e.Hoster)
+            .Where(e => e.Hoster.Id == id)
+            .Skip((paginationFilter.PageNumber - 1) * paginationFilter.Count)
+            .Take(paginationFilter.Count)
+            .ToArrayAsync();
+        if (dataSlice is null)
+            return NotFound();
 
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
+        var dto = _mapper.Map<ICollection<GetTogetherDto>>(dataSlice);
 
-            return NoContent();
-        }
+        return Ok(_pagedResponseFactory.CreatePagedReponse(dto, paginationFilter, Request.Path.Value ?? ""));
+    }
 
-        private bool UserExists(int id)
-        {
-            return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
+    [HttpGet("{userId}/hostings/{getTogetherId}")]
+    public async Task<ActionResult<Response<GetTogetherDto>>> GetUserHostingsById(string userId, int getTogetherId)
+    {
+        var user = await _context.Users
+            .Where(u => u.Id == userId)
+            .Include(u => u.HostedGetTogethers)
+            .FirstOrDefaultAsync();
+        if (user is null)
+            return NotFound(new Response<string>("User not found."));
+
+        var getTogether = user.HostedGetTogethers
+            .Where(e => e.Id == getTogetherId)
+            .FirstOrDefault();
+        if (getTogether is null)
+            return NotFound(new Response<string>("User does not host this get together."));
+
+        var dto = _mapper.Map<GetTogetherDto>(getTogether);
+
+        return Ok(new Response<GetTogetherDto>(dto));
+    }
+
+    [HttpGet("{id}/subscriptions")]
+    public async Task<ActionResult<PagedResponse<GetTogetherDto>>> GetUserAllSubscriptions(string id, [FromQuery] PaginationFilter paginationFilter)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user is null)
+            return NotFound(new Response<string>("User not found."));
+
+        var dataSlice = await _context.GetTogethers
+            .Include(e => e.Subscribers)
+            .Where(e => e.Subscribers
+                .Any(s => s.Id == id))
+            .Skip((paginationFilter.PageNumber - 1) * paginationFilter.Count)
+            .Take(paginationFilter.Count)
+            .ToArrayAsync();
+        if (dataSlice is null)
+            return NotFound();
+
+        var dto = _mapper.Map<ICollection<GetTogetherDto>>(dataSlice);
+
+        return Ok(_pagedResponseFactory.CreatePagedReponse(dto, paginationFilter, Request.Path.Value ?? ""));
+    }
+
+    [HttpGet("{userId}/subscriptions/{getTogetherId}")]
+    public async Task<ActionResult<Response<GetTogetherDto>>> GetUserSubscriptionById(string userId, int getTogetherId)
+    {
+        var user = await _context.Users
+            .Where(u => u.Id == userId)
+            .Include(u => u.HostedGetTogethers)
+            .FirstOrDefaultAsync();
+        if (user is null)
+            return NotFound(new Response<string>("User not found."));
+
+        var getTogether = user.HostedGetTogethers
+            .Where(e => e.Id == getTogetherId)
+            .FirstOrDefault();
+        if (getTogether is null)
+            return NotFound(new Response<string>("User has not been subscribed to this get together."));
+
+        var dto = _mapper.Map<GetTogetherDto>(getTogether);
+
+        return Ok(new Response<GetTogetherDto>(dto));
     }
 }
